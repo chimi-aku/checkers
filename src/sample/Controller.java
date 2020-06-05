65b08d1a46c4dfbf70a282b438f8bbc5d8eb12c7
chimi-aku <lukaszterpilowski9@gmail.com>
2020-06-05 17:36:27 +0200

    Mockup of board GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
 public class Controller {
+
+    private Rectangle r7c0;
+
+    @FXML
+    public void click_on_field(MouseEvent e){
+        System.out.print("xD");
+        System.out.print(e.getID());
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -15,18 +16,19 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 525, 600));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        //launch(args);
+
+        launch(args);
 
         Board board = new Board();
         board.setup_board();
 
 
-        board.move_piece(board.get_piece(2,0));
+        //board.move_piece(board.get_piece(0,7));
     }
 }
